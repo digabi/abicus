33b7ef339aab606f3380b1ca832609fe90e3c67c
Emma Koskinen <25252659+emsknock@users.noreply.github.com>
2024-07-04 17:38:13 +0300

    Use hashmark for imports from src

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@
 
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
-	plugins: [react()],
+	plugins: [tsconfigPaths(), react()],
 
 	// Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
 	//
