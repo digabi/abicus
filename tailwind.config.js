e1c8a19b599c87a3bc310c52ddd4a6b1e995dcb0
Emma Koskinen <25252659+emsknock@users.noreply.github.com>
2024-07-15 11:04:06 +0300

    Add Abi colours

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
 	content: ["./index.html", "./src/**/*.{ts,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				"abi-lblue": "#CCDFFD",
+				"abi-dblue": "#365488",
+				"abi-lgrey": "#E5EAF1",
+				"abi-dgrey": "#C5C5C5",
+			},
+		},
 	},
 	plugins: [],
 };
