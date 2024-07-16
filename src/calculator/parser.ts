73844682d09bb9d5c35103146318d66102e798f2
Emma Koskinen <25252659+emsknock@users.noreply.github.com>
2024-07-16 11:06:31 +0300

    Add a basic syntax check

--- src/calculator/parser.ts
+++ src/calculator/parser.ts
@@ -1,4 +1,4 @@
-import { isMatching, match } from "ts-pattern";
+import { isMatching, match, P } from "ts-pattern";
 import { Token, TokenId } from "./tokeniser";
 
 /**
@@ -19,6 +19,8 @@ import { Token, TokenId } from "./tokeniser";
  * @see Shunting Yard Algorithm: {@link https://en.wikipedia.org/wiki/Shunting_yard_algorithm}
  */
 export default function parse(tokens: Token[]) {
+	if (!syntaxCheck(tokens)) return;
+
 	const outputStack: Token[] = []; // Final RPN stack
 	const sidingStack: Token[] = []; // AKA the operator stack
 
@@ -73,6 +75,64 @@ export default function parse(tokens: Token[]) {
 	}
 
 	return outputStack;
+}
+
+/**
+ * A basic syntax check.
+ *
+ * Iterates over the tokens in the expression and checks rules like "are the brackets balanced"
+ * and that all operators have operands.
+ *
+ * Returns `true` if the input passes and `false` if there is a syntax error somewhere in the input.
+ */
+function syntaxCheck(tokens: Token[]) {
+	// Bracket balance:
+	let bracketStackSize = 0;
+	for (const token of tokens) {
+		bracketStackSize += match(token.type)
+			.with("lbrk", () => 1)
+			.with("rbrk", () => -1)
+			.otherwise(() => 0);
+
+		// All right brackets must have a left bracket somewhere:
+		if (bracketStackSize < 0) return false;
+	}
+
+	if (bracketStackSize !== 0) return false;
+
+	// Shorthands to avoid writing these over and over:
+	const { any, union, not } = P;
+	const oper = { type: "oper" satisfies TokenId } as const;
+	const func = { type: "func" satisfies TokenId } as const;
+	const lbrk = { type: "lbrk" satisfies TokenId } as const;
+	const rbrk = { type: "rbrk" satisfies TokenId } as const;
+	const litr = { type: "litr" satisfies TokenId } as const;
+	const cons = { type: "cons" satisfies TokenId } as const;
+	const memo = { type: "memo" satisfies TokenId } as const;
+
+	// Basic syntax rules:
+	for (let i = 0; i < tokens.length; i++) {
+		const lhs = tokens[i - 1] ?? null;
+		const cur = tokens[i]!;
+		const rhs = tokens[i + 1] ?? null;
+
+		const hasSyntaxError = match([lhs, cur, rhs])
+			// Operators must have operand on their left side:
+			.with([null, oper, any], () => true)
+			// Operators must have operand on their right side:
+			.with([any, oper, null], () => true)
+			// Function name must have left bracket on its right side:
+			.with([any, func, not(lbrk)], () => true)
+			// Number literal, constant, or memory reference can only have operator or left bracket on its left side:
+			.with([not(union(null, oper, lbrk)), union(litr, cons, memo), P.any], () => true)
+			// Number literal, constant, or memory reference can only have operator or right bracket on its right side:
+			.with([any, union(litr, cons, memo), not(union(oper, rbrk, null))], () => true)
+			.otherwise(() => false);
+
+		if (hasSyntaxError) return false;
+	}
+
+	return true;
 }
 
 function precedence(token: Token<"oper">): number {
