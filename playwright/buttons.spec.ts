f546ae82dc2db223f42f1ddd94e2153941179907
Emma Koskinen <25252659+emsknock@users.noreply.github.com>
2024-07-26 14:25:27 +0300

    Test all buttons at startup

--- playwright/buttons.spec.ts
+++ playwright/buttons.spec.ts
@@ -4,14 +4,17 @@ test.beforeEach(async ({ page }) => {
 	await page.goto("/");
 });
 
-test("Has trigonometric buttons", async ({ page }) => {
-	for (const name in ["sin", "cos", "tan", "arcsin", "arccos", "arctan"]) {
-		await expect(page.getByRole("button", { name, exact: true })).toBeVisible();
-	}
-});
+function expectButtons(title: string, names: string[]) {
+	test(title, async ({ page }) => {
+		await Promise.all(names.map(name => expect(page.getByRole("button", { name, exact: true })).toBeVisible()));
+	});
+}
 
-test("Has digit buttons", async ({ page }) => {
-	for (const name in [..."0123456789"]) {
-		await expect(page.getByRole("button", { name, exact: true })).toBeVisible();
-	}
-});
+expectButtons("Has trigonometric buttons", ["sin", "cos", "tan", "arcsin", "arccos", "arctan"]);
+expectButtons("Has digit buttons", [..."0123456789"]);
+expectButtons("Has constant buttons", ["π", "e"]);
+expectButtons("Has decimal, operator, and bracket buttons", [...",+−×/()"]);
+expectButtons("Has memory buttons", ["Min", "Mout", "ANS"]);
+expectButtons("Has logarithm buttons", ["log", "ln"]);
+expectButtons("Has exponent and root buttons", ["x2", "xy", "×10x", "√"]);
+expectButtons("Has del, clear, and calculate buttons", ["⌫", "AC", "="]);
